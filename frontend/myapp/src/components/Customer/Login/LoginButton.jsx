7c58d5197adc571d8d4b2c468a3754fbef050b9e
sontruonghp2004 <tranvutruongson14062004@gmail.com>
2025-02-20 11:11:51 +0700

    logijbutton

--- frontend/myapp/src/components/Customer/Login/LoginButton.jsx
+++ frontend/myapp/src/components/Customer/Login/LoginButton.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
+import { FaLock } from "react-icons/fa";
 import { Link } from 'react-router-dom';
 
 const LoginButton = () => {
   return (
-    <>
+    <div>
       <style>
         {`
           .login-button {
+            background-color: #ff69b4; /* Pink color */
             border: none;
             color: #fff;
             font-weight: 500;
@@ -15,17 +17,31 @@ const LoginButton = () => {
             border-radius: 5px;
             display: inline-block;
             text-align: center;
+            transition: background 0.3s;
           }
 
           .login-button:hover {
+            background-color: #ff85c1; /* Lighter pink color */
             color: #fff;
+          }
+
+          .login-button .icon {
+            margin-right: 8px;
+          }
+
+          .login-button .text {
+            font-size: 16px;
+            font-weight: bold;
           }
         `}
       </style>
       <Link to="/login" className="login-button">
-        Đăng nhập
+        <div style={{ display: "flex", alignItems: "center" }}>
+          <FaLock className="icon" />
+          <span className="text">Login</span>
+        </div>
       </Link>
-    </>
+    </div>
   );
 };
 
--- frontend/myapp/src/components/HomePage/Navbarr.jsx
+++ frontend/myapp/src/components/HomePage/Navbarr.jsx
@@ -39,10 +39,10 @@ function Navbarr() {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="mx-auto">
-            <Nav.Link as={Link} to="/">Trang chủ</Nav.Link>
-            <Nav.Link href="#features">Cộng Đồng</Nav.Link>
+            <Nav.Link as={Link} to="/">Home</Nav.Link>
+            <Nav.Link href="#features">Community</Nav.Link>
             <Nav.Link href="about">About Us</Nav.Link>
-            <Nav.Link href="#tracking">Theo dõi</Nav.Link>
+            <Nav.Link href="#tracking">Follow</Nav.Link>
             <Nav.Link href="membership">Member</Nav.Link>
             <Nav.Link href="blog">Blog</Nav.Link>
             <Nav.Link href="contact">Contact</Nav.Link>
@@ -50,7 +50,7 @@ function Navbarr() {
           <Nav className="ms-auto">
             {isLoggedIn ? (
               <Nav.Link onClick={handleLogout} className="text-danger">
-                Đăng xuất
+                Logout
               </Nav.Link>
             ) : (
               <>
