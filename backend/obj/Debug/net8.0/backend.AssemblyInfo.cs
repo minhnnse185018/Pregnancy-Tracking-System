8fda4257d67d756368e986108b3bbd569785e7da
Blaosak <minhnnse185018@fpt.edu.vn>
2025-03-06 02:11:37 +0700

    done blog without image

--- backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+77169b8f5d053925c3f20f7e56c75e787e06b654")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2af68f46b010796d16a6668187e576b9f3eab7c5")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
