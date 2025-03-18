88ae1b61f76099c0b191e64492038d5f7b59fbc8
lloocc11 <locttptse184628@fpt.edu.vn>
2025-03-18 23:22:42 +0700

    cloudinary

--- backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fe84afa43638b029b33a700fac347fc619c733fa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e0079657bdaded41d5a52f6b7a2c3482e159c168")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
